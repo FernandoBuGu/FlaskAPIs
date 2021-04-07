dbb496f8a3361f2d079ca1ce07c598e7a6309005
Fernando Bueno Gutiérrez <fernando.buenogutierrez@wur.nl>
2021-04-07 17:41:44 +0200

    Update serve_model.sh

--- catsVSdogs/serve_model.sh
+++ catsVSdogs/serve_model.sh
@@ -1,1 +1,1 @@
-docker run -p 8502:8501 --name=pets -v "/home/models/pets/:/models/pets/1" -e MODEL_NAME=pets tensorflow/serving
+sudo docker run -p 8502:8501 --name=pets -v "/home/febueno/Documents/mCvsD/pets/:/models/pets/1" -e MODEL_NAME=pets tensorflow/serving
